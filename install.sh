00bea9ac38d4463e045b0b7ed0b273bad7bb2b4d
François-Xavier Thomas <fx.thomas@gmail.com>
2013-07-01 15:05:55 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Base Script File (install.sh)
+# Created: Mon Jul  1 15:03:41 2013
+# Version: 1.0
+# Author: François-Xavier Thomas <fx.thomas@gmail.com>
+#
+# This Bash script was developped by François-Xavier Thomas.
+# You are free to copy, adapt or modify it.
+# If you do so, however, leave my name somewhere in the credits, I'd appreciate it ;)
+
+ln -s .bash_profile ~/
+ln -s .bashrc ~/
+ln -s .gdbinit ~/
+ln -s .git ~/
+ln -s .gitconfig ~/
+ln -s .gitignore ~/
+ln -s .gitignore_global ~/
+ln -s .gitmodules ~/
+ln -s .install.sh.swp ~/
+ln -s .lftp ~/
+ln -s .tmux.conf ~/
+ln -s .vim ~/
+ln -s .vimrc ~/
+ln -s .zshrc ~/
+ln -s .zshrc_aliases ~/
+ln -s fx.zsh-theme ~/.oh-my-zsh/themes/
