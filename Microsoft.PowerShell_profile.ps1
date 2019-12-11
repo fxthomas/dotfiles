4934fd1df0c1509db636ab98884769518eeb6175
François-Xavier Thomas <fxthomas@dxomark.com>
2019-12-11 14:24:26 +0100

    PS: Add function for loading Python C++ compiler tools

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,58 @@
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+Import-Module 'C:\Users\fxthomas\Documents\WindowsPowerShell\Modules\ListProjectTasks.psm1'
+Import-Module posh-git
+
+# This function configures the current PowerShell console for using Visual
+# C++ builds for Python extensions on the command line.
+#
+# Example:
+#
+#   Set-PyVsCmd 2.7
+#
+function Set-PyVsCmd
+{
+    param(
+        [parameter(Mandatory=$true, HelpMessage="Enter Python version as 2.7")]
+        [ValidateSet("2.7")]
+        [string]$version,
+        [parameter(Mandatory=$true, HelpMessage="Enter architecture as x86, x64")]
+        [ValidateSet("x86","x64")]
+        [string]$arch
+    )
+
+    switch ($arch)
+    {
+      "x86" { $vcvars = "vcvars32.bat" }
+      "x64" { $vcvars = "vcvars64.bat" }
+      default { $vcvars = "vcvarsall.bat" }
+    }
+
+    $VS_VERSION = @{ "2.7" = "9.0" }
+    switch ($version)
+    {
+      "2.7" { $targetDir = "c:\Program Files (x86)\Microsoft Visual Studio $($VS_VERSION[$version])\VC\bin" }
+    }
+
+    if (!(Test-Path (Join-Path $targetDir $vcvars))) {
+        "Error: Visual Studio $version not installed"
+        return
+    }
+    pushd $targetDir
+    cmd /c "$vcvars $arch $winsdkver & set" |
+    foreach {
+      if ($_ -match "(.*?)=(.*)") {
+        Set-Item -force -path "ENV:\$($matches[1])" -value "$($matches[2])"
+      }
+    }
+    popd
+
+    # See: https://stackoverflow.com/a/29461265/821995
+    Set-Item -force -path "ENV:DISTUTILS_USE_SDK" -value "1"
+    Set-Item -force -path "ENV:MSSdk" -value "1"
+
+    write-host "`nVisual Studio for Python $version ($arch) Command Prompt variables set." -ForegroundColor Yellow
+}
+
 # This function configures the current PowerShell console for using Visual
 # Studio command-line tools ("Developper Command Prompt").
 #
@@ -9,11 +64,10 @@
 # See: http://putridparrot.com/blog/setup-powershell-to-use-the-visual-studio-paths-etc/
 # See: https://stackoverflow.com/questions/2124753/how-can-i-use-powershell-with-the-visual-studio-command-prompt
 # See: https://msdn.microsoft.com/en-us/library/f2ccy3wt.aspx
-
 function Set-VsCmd
 {
     param(
-        [parameter(Mandatory=$true, HelpMessage="Enter VS version as 2010, 2012, 2013, 2015, 2017")]
+        [parameter(Mandatory=$true, HelpMessage="Enter VS version as 2008, 2010, 2012, 2013, 2015, 2017")]
         [ValidateSet(2008,2010,2012,2013,2015,2017)]
         [int]$version,
         [parameter(Mandatory=$true, HelpMessage="Enter architecture as x86, x64")]
@@ -45,11 +99,19 @@ function Set-VsCmd
       }
     }
     popd
+
+    # See: https://stackoverflow.com/a/29461265/821995
+    Set-Item -force -path "ENV:DISTUTILS_USE_SDK" -value "1"
+    Set-Item -force -path "ENV:MSSdk" -value "1"
+
     write-host "`nVisual Studio $version ($arch) Command Prompt variables set." -ForegroundColor Yellow
 }
 
 # Load developper tools for VS 2015 64-bits, with Windows SDK 8.1
 # Set-VsCmd 2015 x64 8.1
+
+# Load developper tools for Python compilation
+# Set-PyVsCmd 2.7 x64
 
 # Set aliases for python
 function python2() { py -2 $args }
